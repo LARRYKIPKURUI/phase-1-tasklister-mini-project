493b12b5ae764acc1bc73178ed47a7c4d8f4ec1d
LARRY KIPKURUI <larrykipkurui12@gmail.com>
2025-03-16 19:35:52 +0300

    Created To do list

--- src/index.js
+++ src/index.js
@@ -1,3 +1,32 @@
+
 document.addEventListener("DOMContentLoaded", () => {
-  // your code here
+
+  document.getElementById('create-task-form').addEventListener('submit', function (event) {
+    event.preventDefault();
+    
+    const taskInput = document.getElementById('new-task-description');
+    const taskText = taskInput.value.trim(); //Trim input just incase of whitespaces
+
+    if (taskText === '') return;
+
+    // Create list item (to add content to the list)
+    const li = document.createElement('li');
+    li.textContent = taskText;
+    li.classList.add('p-2', 'mt-2');
+
+    // Create Remove Button
+    const removeButton = document.createElement('button');
+    removeButton.textContent = 'Remove';
+    removeButton.classList.add('p-2', 'btn', 'btn-danger','mx-5');
+
+    // Append the removing  button to list item
+    li.appendChild(removeButton);
+    document.getElementById('tasks').appendChild(li);
+
+    // Attach event to remove only the clicked item by listening to event click !
+    removeButton.addEventListener('click', () => li.remove());
+
+    // Clear input field for input
+    taskInput.value = '';
+  });
 });
